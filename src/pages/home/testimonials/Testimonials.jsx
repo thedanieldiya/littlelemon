73eab50093a6fc31aaa1c9c1af56b780d8d034ac
thedanieldiya <thedanieldiya@gmail.com>
2024-10-19 22:41:28 +0100

    specials layout

---
+++ src/pages/home/testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./testimonials.css";
+
+const Testimonials = () => {
+	return (
+		<div className="testimonials">
+			<section className="section"></section>
+		</div>
+	);
+};
+
+export default Testimonials;
